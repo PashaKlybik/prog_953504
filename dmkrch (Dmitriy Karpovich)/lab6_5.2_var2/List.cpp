6e53f3811dfe47f071f092c23bf165a0eba51fee
Dmitriy Karpovich <dmkrch1@gmail.com>
2020-05-14 21:01:22 +0300

    added 5, 6, 7 labs

---
+++ dmkrch (Dmitriy Karpovich)/lab6_5.2_var2/List.cpp
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+#include "List.h"
+
+
+DoubleLinkedList* CreateDblLinkedList()
+{
+    DoubleLinkedList* tmp = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));
+    tmp->size = 0;
+
+    tmp->head = tmp->tail = NULL;
+
+    return tmp;
+}
+
+void PushNode(DoubleLinkedList* list, int* arr, int arrSize, int treeNumber)
+{
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    if (tmp == NULL)
+    {
+        exit(3);
+    }
+    tmp->arr = arr;
+    tmp->treeNumber = treeNumber;
+    tmp->next = NULL;
+    tmp->arrSize = arrSize;
+
+    tmp->prev = list->tail;
+    if (list->tail)
+    {
+        list->tail->next = tmp;
+    }
+    list->tail = tmp;
+
+    if (list->head == NULL)
+    {
+        list->head = tmp;
+    }
+    list->size++;
+}
