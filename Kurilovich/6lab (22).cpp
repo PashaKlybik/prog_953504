295e04f4a4f09eac2c0711e3240c39a1f7f0f360
Karina <kartinezmiss@gmail.com>
2020-05-23 12:04:40 +0300

    kurilovich

---
+++ Kurilovich/6lab (22).cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+
+struct tree
+{
+    int inf;
+    tree* l = nullptr;
+    tree* r = nullptr;
+};
+
+struct Stack
+{
+    int num;
+    Stack* next = nullptr;
+    Stack* prev = nullptr;
+};
+
+void Add(Stack **top, int n)
+{
+    Stack* newStack = new Stack();
+    newStack->num = n;
+    newStack->prev = nullptr;
+    newStack->next = nullptr;
+    Stack *run = *top;
+
+    if (run == nullptr)
+    {
+        *top = newStack;
+    }
+    else
+    {
+        newStack->next = *top;
+        run->prev = newStack;
+        *top = newStack;
+    }
+}
+
+void In_Order(tree* tre, Stack **r, Stack **l) {
+    if (tre == nullptr) { return; }
+    if (tre->inf % 2 == 0) {
+        Add(&*r, tre->inf);
+    }
+    else {
+        Add(&*l, tre->inf);
+    }
+    In_Order(tre->l, r, l);
+    In_Order(tre->r, r, l);
+}
+
+void Add(tree** tre, int value) {
+
+    tree* branch = new tree();
+    branch->inf = value;
+    branch->l = nullptr;
+    branch->r = nullptr;
+    tree* root = &**tre;
+
+    if (*tre == nullptr) {
+        *tre = branch;
+    }
+    else {
+        while (true) {
+            if (root->inf < value) {
+                if (root->r == nullptr) {
+                    root->r = branch;
+                    return;
+                }
+                root = root->r;
+            }
+            else {
+                if(root->l == nullptr) {
+                    root->l = branch;
+                    return;
+                }
+                root = root->l;
+            }
+        }
+    }
+    
+}
+
+void Print(Stack* top) {
+    while (top != nullptr) {
+        printf("   %d", top->num);
+        top = top->next;
+    }
+}
+
+
+int main()
+{
+    tree* tre = nullptr;
+    Stack* r = nullptr;
+    Stack* l = nullptr;
+
+    Add(&tre, 5);
+    Add(&tre, 1);
+    Add(&tre, 2);
+    Add(&tre, 3);
+    Add(&tre, 4);
+    Add(&tre, 6);
+    Add(&tre, 7);
+    Add(&tre, 8);
+    Add(&tre, 9);
+    Add(&tre, 10);
+
+    In_Order(tre, &r, &l);
+
+    Print(r);
+    printf("\n");
+    Print(l);
+
+    system("pause");
+    return 0;
+}
