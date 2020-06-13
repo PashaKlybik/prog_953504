52f90c18ce160fa638e0c2e4e45ef29ef101c75f
IgorGerasimuk <61730273+IgorGerasimuk@users.noreply.github.com>
2020-06-13 20:42:42 +0300

    Add files via upload

---
+++ IgorGerasimuk/laba2.2.cpp
@@ -0,0 +1,102 @@
+﻿
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <locale>
+
+void iteration(double, double);
+void recursive(double, double);
+double func(double, double, double);
+
+int count = 1;
+
+long int factorial(long int n)
+{
+	if (n == 0 || n == 1) return 1;
+	return n * factorial(n - 1);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	double x;
+	double error;
+	int operation;
+	while (true) {
+		printf("Введите нужную вам операцию: \n");
+		printf("1. Итерационный метод:\n");
+		printf("2. Рекурсивный метод:\n");
+		printf("3. Выход\n\n");
+		scanf_s("%d", &operation);
+
+		printf("Введите e: ");
+		scanf_s("%le", &error);
+
+		printf("e: %lf\n", error);
+
+		printf("Введите X: ");
+		scanf_s("%le", &x);
+		printf("\nSIN(x) = %f\n", sin(x));
+
+		switch (operation) {
+		case 1: {
+			clock_t begin = clock();
+			iteration(x, error);
+			clock_t end = clock();
+			double time_spend1 = (double)(end - begin);
+			printf("Время выполнения = %.1f", time_spend1);
+			printf(" мс\n");
+			break;
+		}
+		case 2:
+			recursive(x, error);
+			count = 1;
+			break;
+		case 3:
+			exit(0);
+			break;
+		}
+	}
+}
+
+void iteration(double x, double error) {
+
+	double decompos = 0;
+
+	int n = 0;
+	int k = 1;
+	do {
+		decompos += -1 * ((pow(-1, k) * pow(x, 2 * k - 1)) / factorial(2 * k - 1));
+		n++;
+		k++;
+	} while ((decompos - sin(x)) > error);
+
+	printf("\nПогрешность е: %f\n", decompos - sin(x));
+	printf("Итераций: %d\n\n", n);
+}
+
+void recursive(double x, double error) {
+
+	double decompos = 0;
+	clock_t begin = clock();
+	decompos = func(decompos, x, error);
+
+	printf("\nПогрешность e: %f\n", decompos - sin(x));
+	printf("Итераций: %d\n\n", count - 1);
+
+	clock_t end = clock();
+	double time_spend = (double)(end - begin);
+	printf("Времени затрачено: %.1f", time_spend);
+	printf(" мс\n");
+}
+
+double func(double decompos, double x, double error) {
+
+	decompos += -1 * ((pow(-1, count) * pow(x, 2 * count - 1)) / factorial(2 * count - 1));
+	count++;
+
+	if (abs((decompos - sin(x)) < error)) return decompos;
+
+	return func(decompos, x, error);
+}
---
+++ IgorGerasimuk/laba2.cpp
@@ -0,0 +1,91 @@
+﻿#include <stdio.h>
+#include <locale>
+
+void ball_surface_area(float radius, float ball_area, float PI);
+void ball_volume(float radius, float PI);
+void tetrahedron_edge_length(float radius);
+void cross_sectional_area(float radius, float PI);
+void Feedback();
+
+void main(void)
+{
+	setlocale(LC_ALL, "russian");
+	float radius = 0;
+	float ball_area = 0;
+	float PI = 3.14;
+	int function;
+	while (true)
+	{
+		printf("----------------------------------");
+		printf("\nВыберите нужную вам функцию:\n");
+		printf("1. Ввод радиуса шара\n");
+		printf("2. Вывод площади поверхности шара\n");
+		printf("3. Вывод объема шара\n");
+		printf("4. Вывод длины ребра вписанного правильного тетраэдра\n");
+		printf("5. Вывод площади сечения, проведенного на расстоянии k от центра шара\n");
+		printf("6. Информация о версии и автора программы\n");
+		printf("7. Выход\n");
+
+		scanf_s("%d", &function);
+		printf("----------------------------------\n");
+
+		switch (function)
+		{
+		case 1:
+			printf("Введите радиус шара: ");
+			scanf_s("%f", &radius);
+			printf("\nРадиус шара: %.2f\n", radius);
+			break;
+		case 2:
+			ball_surface_area(radius, ball_area, PI);
+			break;
+		case 3:
+			ball_volume(radius, PI);
+			break;
+		case 4:
+			tetrahedron_edge_length(radius);
+			break;
+		case 5:
+			cross_sectional_area(radius, PI);
+			break;
+		case 6:
+			Feedback();
+			break;
+		case 7:
+			exit(0);
+		}
+	}
+}
+
+void ball_surface_area(float radius, float ball_area, float PI) {
+	ball_area = 4 * PI * pow(radius, 2);
+	printf("Радиус шара равна: %.2f\n", radius);
+	printf("\nПлощадь поверхности шара равна: %.2f\n", ball_area);
+}
+
+void ball_volume(float radius, float PI) {
+	float Volume;
+	Volume = 4 / 3 * PI * pow(radius, 3);
+	printf("Объем шара равен: %.2f\n", Volume);
+}
+
+void tetrahedron_edge_length(float radius) {
+	float edge;
+	edge = 4 * radius / sqrt(6);
+	printf("Длина ребра вписанного тетраэдра: %.2f\n", edge);
+}
+
+void cross_sectional_area(float radius, float PI) {
+	float k;
+	float section_area;
+	printf("Введите расстояние от центра шара: ");
+	scanf_s("%f", &k);
+	section_area = PI * pow(k, 2);
+	printf("\nПлощадь сечения, проведенного на расстоянии k от центра шара, равна: %.2f\n", section_area);
+}
+
+void Feedback() {
+	printf("\nВерсия программы: 1.1.0\n");
+	printf("Автор: Герасимук Игорь Викторович\n");
+	printf("Kонтактная информация: +375 44 123 45 67\n");
+}
