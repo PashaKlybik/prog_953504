9a2db3ce45a15b4b15ad295181316c13d942d4d4
IgorGerasimuk <61730273+IgorGerasimuk@users.noreply.github.com>
2020-06-13 21:12:19 +0300

    Add files via upload
    Commit includes files from labs 4.1 and 4.2 variant 17

---
+++ IgorGerasimuk/laba4.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <fstream>
+#include <string>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+int main()
+{
+
+    ifstream infile("text1.txt");
+    ofstream outfile("text2.txt", ios::trunc);
+
+    string buffer;
+
+    while (getline(infile, buffer))//буффер под строку
+    {
+        if (buffer > "P", "M", "A", "C", "O", "B", "E", "K" && buffer < "П")
+
+            outfile << buffer << endl;//записываем строку в выходной файл
+
+    }
+
+    infile.close();//закрываем
+    outfile.close(); //файлы
+
+    return 0;
+}
