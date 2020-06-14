6aa7d15895633d3d6ded2a0d6dfd9eb4f9978500
IgorGerasimuk <61730273+IgorGerasimuk@users.noreply.github.com>
2020-06-14 18:01:29 +0300

    [IgorGerasimuk] lab4.1 and 4.2 variant-17 (#106)
    * Create labs 1-7
    
    Folder include LR 1-7 from Igor Gerasimuk
    
    * Add files via upload
    
    Commit includes files from labs 4.1 and 4.2 variant 17

---
+++ IgorGerasimuk/laba4.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <malloc.h>
+
+using namespace std;
+
+int main()
+{
+    char* str = (char*)malloc(128);
+    puts(str);
+
+    int outbkt = true;
+    int nobkt = true;
+    while (*str != '\0')
+    {
+        if (*str == '(') { printf("%c", *str++); outbkt = false; nobkt = false; }
+        if (*str == ')') { printf("%c", *str++); outbkt = true; }
+        if (outbkt)printf("%c", *str++);
+        if (!outbkt)str++;
+    }
+    printf("\n");
+    if (!outbkt)printf("Error: in input, no ')'\n");
+    if (nobkt)printf("Error: no brekets was in input line");
+
+    _getch();
+    return 0;
+}
