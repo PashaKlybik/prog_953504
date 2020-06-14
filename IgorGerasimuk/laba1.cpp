d332e9c7cc69a237b38e759b0adbae3728a0ea03
IgorGerasimuk <61730273+IgorGerasimuk@users.noreply.github.com>
2020-06-14 17:58:08 +0300

    [IgorGerasimuk] lab1 variant-29 (#103)
    * Create labs 1-7
    
    Folder include LR 1-7 from Igor Gerasimuk
    
    * Add files via upload
    
    The file include lab1

---
+++ IgorGerasimuk/laba1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "stdio.h"
+#include "math.h"
+#include <stdlib.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int n;
+
+	printf("vvedite chislo: ");
+	scanf_s("%d", &n);
+
+	char ch[20];
+
+	_itoa_s(n, ch, 10);
+	bool flag1 = false;
+	bool flag2 = false;
+	for (int i = 0; i != strlen(ch) - 1; i++)
+	{
+		if (ch[i] > ch[i + 1])
+			flag1 = true;
+
+		if (ch[i] < ch[i + 1])
+			flag2 = true;
+
+		if (flag2 == flag1)
+			break;
+	}
+	if (!flag2 == !flag1)
+		printf("ne po vozrastaniju i ne po ubivaniu");
+	else
+		if (!flag1)
+			printf("vse cifri uporjadocheni po vozrastaniju ");
+		else
+			printf("vse cifri uporjadocheni po ubivaniu ");
+}
